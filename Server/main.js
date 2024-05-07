6ae3c4245f44f18fdcff3328d7cd3e3e2b2c9996
SoryQ14 <squibtanarosas64@gmail.com>
2024-05-07 09:47:56 -0600

    primer commit

---
+++ Server/main.js
@@ -0,0 +1,17 @@
+
+var express = require('express'); 
+var app = express(); 
+
+/** Como trabajaremos con socket, es recomendable usar el modulo HTTP para upasarle la app a express y manekar bien http */
+var server = require ('http').Server(app); 
+
+/** Aqui estará toda la funcionallidad de los sockets se requiere la libreria socket.io, se pasa la variable Server que tiene la app express y HTTP */
+var io = require('socket.io')(server); 
+
+app.get('/',function(req, res){
+    res.status(200).send("Hola Mundo :)"); 
+}); 
+
+server.listen(3009, function(){
+    console.log("El servidor está corriendo en el http://localhost:3009");
+});
