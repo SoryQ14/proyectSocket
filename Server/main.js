e055c1805483c0a456b99cd1908a91f1ad26b03e
SoryQ14 <squibtanarosas64@gmail.com>
2024-05-13 09:55:44 -0600

    versio1.0.4

--- Server/main.js
+++ Server/main.js
@@ -3,6 +3,12 @@ var app = express();
 var server = require ('http').Server(app); 
 var io = require('socket.io')(server); 
 
+var messages = [{
+    id: 1,
+    texto: "Hola soy un mensaje",
+    Autor: "Soraja Quintana",
+}]; 
+
 /**Usamos un diffleware para usar elementos estaticos en las seccion publica  */
 app.use(express.static('public')); 
 
@@ -12,12 +18,12 @@ app.get('/',function(req, res){
 
 io.on('connection', function(socket){
     console.log('Alguien conectado con socket')
-    /**Aqui controlamos los evento del cliente mediante sockets */
-    socket.emit('messages',{
-        id: 1,
-        texto: "Hola soy un mensaje",
-        Autor: "Soraja Quintana",
-    });
+    socket.emit('messages', messages); 
+    socket.on('new-message', function(data){
+    messages.push(data); 
+    //queremos que todos los mensajes se manden a todos los clientes
+    io.sockets.emit('messages', messages); 
+    }); 
 });
 
 server.listen(3009, function(){
--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
 /**Creamos la variable que permitira al frontend conectarse a nuestro backend */
 
-const { text } = require("express");
+//const { text } = require("express");
 
 var socket = io.connect('http://localhost:3009',{'forceNew': true}); 
 
@@ -13,13 +13,15 @@ socket.on('messages', function(data){
 
 /**Creamos un template para que nos imprima el conenido */
 function render(data){
-    //aqui se inicia el manejo de variables string que viene en EM6 se usan estas comillas ``
-    //Las variables se colocan con el signo de $ y entre {}
-    var html = `<div>
-                    <strong> ${data.Autor}</strong>
-                    <em>${data.texto}</em>
-                </div>`;
-                document.getElementById('messages').innerHTML = html; 
+    //reestructuremos esta seccion ara que se maneje el array, elem es conjunto de cosas 
+    //con map recorremos el array
+    var html = data.map(function(elem, index){
+        return(`<div>
+                    <strong>${elem.Autor}</strong>: 
+                    <em>${elem.texto}</em>
+                    </div>`);
+    }).join(" "); 
+    document.getElementById('messages').innerHTML = html; 
 }
 //cada ves que alguien precione el boton enviar en el formulario
 //El cliente emite un nuevo mensaje y manda el payload
